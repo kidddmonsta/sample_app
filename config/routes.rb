0c784c4e611749a3a1da88e839e71317e46f1cae
kidddMonsta <nikolas.yeap@gmail.com>
2019-05-08 23:13:52 +0300

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
+  #get 'password_resets/new'
 
-  get 'password_resets/edit'
+  #get 'password_resets/edit'
 
   #get 'sessions/new'
 
@@ -28,6 +28,9 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
