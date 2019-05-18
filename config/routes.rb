5e4c7de1711c737cec965bdfd2c06829d4bacf3e
kidddMonsta <nikolas.yeap@gmail.com>
2019-05-18 14:05:49 +0300

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 Rails.application.routes.draw do
 
   #get 'password_resets/new'
@@ -25,11 +26,15 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
-  
+  resources :relationships,       only: [:create, :destroy]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
